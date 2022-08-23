dea3d44ed55886ff3eec6cccfeaa1cd4f39215e7
DEAIFISH <1277676309@qq.com>
2022-08-24 02:59:39 +0800

    2022/08/24

---
+++ MyBatis_mbg/src/test/java/MBG_Test.java
@@ -0,0 +1,25 @@
+import bean.Emp;
+import mapper.EmpMapper;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+
+import static util.SqlSessionFactoryUtils.getSqlSessionFactory;
+
+public class MBG_Test {
+    SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+
+
+    @Test
+    public void test1() {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+
+        Emp emp = mapper.selectByPrimaryKey(1);
+
+        System.out.println(emp);
+
+        sqlSession.close();
+    }
+}
