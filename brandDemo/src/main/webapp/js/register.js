a913602b8a08c5ee387f2a85023fc01860dec88b
DEAiFISH <DEAiFISH@Outlook.com>
2023-11-02 09:27:55 +0800

    2023/10/31

--- brandDemo/src/main/webapp/js/register.js
+++
@@ -1,9 +0,0 @@
-const changeImg1 = document.getElementById("changeImg1");
-const changeImg2 = document.getElementById("changeImg2");
-
-changeImg1.onclick = function () {
-    document.getElementById("checkCodeImg").src = "/brandDemo/checkCodeServlet?" + new Date().getMilliseconds();
-}
-changeImg2.onclick = function () {
-    document.getElementById("checkCodeImg").src = "/brandDemo/checkCodeServlet?" + new Date().getMilliseconds();
-}
