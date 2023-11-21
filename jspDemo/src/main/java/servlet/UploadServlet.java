e0513e80bee202bb513941af1d3692931101fee1
DEAiFISH <DEAiFISH@Outlook.com>
2023-11-21 19:46:37 +0800

    2023/11/21

---
+++ jspDemo/src/main/java/servlet/UploadServlet.java
@@ -0,0 +1,80 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
+/**
+ * @description TODO
+ *
+ * @author DEAiFISH
+ * @date 2023/11/21 18:35
+ */
+@WebServlet("/uploadServlet")
+@MultipartConfig(maxFileSize = 10 * 1024 * 1024)
+public class UploadServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Part part = req.getPart("file1");
+        if (part == null) {
+            return;
+        }
+        String fileName = part.getSubmittedFileName();
+
+//        String path = req.getServletContext().getRealPath("upload");
+        String path = new File("").getAbsolutePath() + "/" + "upload";    //System.getProperty("user.dir")获取当前模块的根目录
+
+        File dir = new File(path);
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+
+        File file = new File(path + "/" + fileName);
+
+        InputStream is = null;
+        BufferedOutputStream bw = null;
+        try {
+            bw = new BufferedOutputStream(Files.newOutputStream(file.toPath()));
+            is = part.getInputStream();
+            int len;
+            byte[] ch = new byte[1024];
+
+            while ((len = is.read(ch)) != -1) {
+                bw.write(ch, 0, len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (bw != null) {
+                try {
+                    bw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
---
+++ jspDemo/src/main/webapp/multipartConfig/upload.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: qingyuan
+  Date: 2023/11/21
+  Time: 18:35
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="${pageContext.request.contextPath}/uploadServlet" method="post" enctype="multipart/form-data">
+  文件描述：
+  <input type="text"><br>
+  选择文件：
+  <input type="file" name="file1"><br>
+  <input type="submit">
+</form>
+</body>
+</html>
